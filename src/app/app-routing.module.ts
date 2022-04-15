ce2fff3844380eca6569f3bcbc5959017fc894f0
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-15 19:42:58 +0300

    Add routes to home page(landing page)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   {
     path: 'users',
     loadChildren: () =>
       import('./users/users.module').then((m) => m.UsersModule),
   },
-  { path: 'repositories', loadChildren: () => import('./repositories/repositories.module').then(m => m.RepositoriesModule) },
+  {
+    path: 'repositories',
+    loadChildren: () =>
+      import('./repositories/repositories.module').then(
+        (m) => m.RepositoriesModule
+      ),
+  },
 ];
 
 @NgModule({
