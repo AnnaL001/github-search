e520f6e8876b8a58f044a80223936f6c07d56c27
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-15 19:10:29 +0300

    Add routes of feature modules: users and repositories to AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./users/users.module').then((m) => m.UsersModule),
+  },
+  { path: 'repositories', loadChildren: () => import('./repositories/repositories.module').then(m => m.RepositoriesModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
