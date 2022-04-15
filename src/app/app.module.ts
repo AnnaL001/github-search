783185b24bcb25b01ae6130061e9241e36716eed
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-15 19:08:28 +0300

    Add feature modules: users and repositories to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { RepositoriesModule } from './repositories/repositories.module';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule,
+    RepositoriesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
