41bf4b7f196fc751a831544a3279630047e6a681
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-17 13:04:19 +0300

    Add functionality to capture input from search user by username form

--- src/app/component/search-user/search-user.component.ts
+++ src/app/component/search-user/search-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-search-user',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchUserComponent implements OnInit {
 
+  user = {} as User;
+  username = "";
+  @Output() inputUsername = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submitUsername(username: string){
+    this.username = username
+    this.inputUsername.emit(this.username);
+    console.log(this.username);
   }
 
 }
