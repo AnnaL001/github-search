d28e9ec238a9e63badb4dfd4659e04a5c96aad40
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-17 13:23:55 +0300

    Add route linking search user results and their github repositories information

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { UserRepoResultComponent } from './component/user-repo-result/user-repo-result.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
     path: 'users',
     loadChildren: () =>
       import('./users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'users/:username',
+    component: UserRepoResultComponent
   },
   {
     path: 'repositories',
