0a0378ef8546b31fbecf5e9d069c58d401cb03e1
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-17 15:06:31 +0300

    Update custom model class: repository

--- src/app/model/repository.ts
+++ src/app/model/repository.ts
@@ -1,12 +1,12 @@
 export class Repository {
   constructor(
-    public name: string = "",
-    public description: string = "",
-    public owner: string = "",
-    public language: string = "",
-    public branches: number = 0,
-    public forks: number = 0,
-    public commits: number = 0,
-    public url: string = "",
-    public createdAt: Date = new Date()){}
+    public name: string,
+    public description: string ,
+    public owner: string,
+    public language: string,
+    public branches: number,
+    public forks: number,
+    public commits: number,
+    public url: string,
+    public createdAt: Date){}
 }
