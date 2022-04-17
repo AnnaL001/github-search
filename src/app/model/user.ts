db830313fe682a7480ab115272114c762528f173
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-17 15:06:07 +0300

    Update custom model class: user

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -2,12 +2,12 @@ import { Repository } from "./repository";
 
 export class User {
   constructor(
-    public username: string = "",
-    public avatar: string = "",
-    public publicRepos: number = 0,
-    public bio: string = "",
-    public followers: number = 0,
-    public following: number = 0,
-    public createdAt: Date = new Date(),
-    public repos: Array<Repository> = []){}
+    public username: string,
+    public avatar: string,
+    public publicRepos: number,
+    public bio: string,
+    public followers: number,
+    public following: number,
+    public createdAt: Date,
+    public repos: Array<Repository>){}
 }
