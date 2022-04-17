cfb18fc490cae73bcaf7dac4d539b6ad22086196
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-17 13:14:06 +0300

    Add functionality to fetch results based on search by username form input

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { UserResquestService } from '../service/user-resquest.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  user = {} as User;
+  constructor(private userService: UserResquestService) { }
 
   ngOnInit(): void {
   }
 
+  searchUser(username: string){
+    this.user = {} as User;
+    this.userService.sendUserRequest(username);
+    this.user = this.userService.user;
+  }
+
+  isEmpty(user: User){
+    return Object.keys(user).length === 0;
+  }
 }
