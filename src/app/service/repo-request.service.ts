65e067433832fa1e49a7edb4fa6c4fadd87a437b
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-18 09:24:27 +0300

    Add service to fetch github repository data via github api

---
+++ src/app/service/repo-request.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Repository } from '../model/repository';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RepoRequestService {
+
+  repositories = [];
+  constructor(private http: HttpClient) { }
+
+  sendRepoRequest(repoName: string){
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http.get<any>(`https://api.github.com/search/repositories?q=${repoName}+in:name?order=created_at&sort=desc?access_key=${environment.apiKey}`).toPromise().then(
+        response => {
+          console.log(response);
+          this.repositories = response.items.map((repo: any) => new Repository(repo.name, repo.description, repo.owner.login, repo.language, repo.branches, repo.forks, repo.commits, repo.html_url, repo.created_at));
+          resolve();
+        },
+        error => {
+          reject(error);
+        }
+      )
+    });
+    return promise;
+  }
+}
