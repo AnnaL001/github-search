6de5dd8fc1bdb1244eff11ba77c758bb3bdfbe0d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-18 12:24:55 +0300

    Update reference to environment variable

--- src/app/service/user-request.service.ts
+++ src/app/service/user-request.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+
 import { Repository } from '../model/repository';
 import { User } from '../model/user';
 
