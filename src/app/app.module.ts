1c991eb5d2aecf3cf6b2fbda657be31d051430c4
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-18 09:40:57 +0300

    Import loader modules into  root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
-import { RepositoriesModule } from './repositories/repositories.module';
-import { UsersModule } from './users/users.module';
 import { HomeComponent } from './component/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserGeneralInfoComponent } from './component/user-general-info/user-general-info.component';
-import { RepoComponent } from './component/repo/repo.component';
 import { UserRepoResultComponent } from './component/user-repo-result/user-repo-result.component';
+import { RepositoriesComponent } from './component/repositories/repositories.component';
+import { SearchRepoComponent } from './component/search-repo/search-repo.component';
+import { RepoComponent } from './component/repo/repo.component';
+import { UsersComponent } from './component/users/users.component';
+import { SearchUserComponent } from './component/search-user/search-user.component';
+import { UserResultComponent } from './component/user-result/user-result.component';
+import { DatePassedPipe } from './pipe/date-passed.pipe';
+import { HighlightDirective } from './directive/highlight.directive';
+import { FormsModule } from '@angular/forms';
+import { NgxUiLoaderHttpModule, NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @NgModule({
   declarations: [
@@ -18,15 +25,23 @@ import { UserRepoResultComponent } from './component/user-repo-result/user-repo-
     NavbarComponent,
     HomeComponent,
     UserGeneralInfoComponent,
+    UserRepoResultComponent,
+    RepositoriesComponent,
+    SearchRepoComponent,
     RepoComponent,
-    UserRepoResultComponent
+    UsersComponent,
+    SearchUserComponent,
+    UserResultComponent,
+    DatePassedPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxUiLoaderModule,
+    NgxUiLoaderHttpModule.forRoot({showForeground: true}),
     HttpClientModule,
-    UsersModule,
-    RepositoriesModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
