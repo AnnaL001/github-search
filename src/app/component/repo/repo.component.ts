e3c57082f27c2f00648661855255508ca0bd3e00
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-18 09:38:15 +0300

    Add functionality of highlighting the most forked repository

--- src/app/component/repo/repo.component.ts
+++ src/app/component/repo/repo.component.ts
@@ -4,14 +4,16 @@ import { Repository } from 'src/app/model/repository';
 @Component({
   selector: 'app-repo',
   templateUrl: './repo.component.html',
-  styleUrls: ['./repo.component.css']
+  styleUrls: ['./repo.component.css'],
 })
 export class RepoComponent implements OnInit {
-
   @Input() repos = [] as Repository[];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getHighestForks(repos: Repository[]) {
+    let highestForks = Math.max(...repos.map((repo) => repo.forks), 1);
+    return highestForks;
   }
-
 }
