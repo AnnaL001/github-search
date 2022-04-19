3e6f51544b2c7649d93d9392a3bc99381953077d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-19 12:49:39 +0300

    Update functionality y to compute date difference

--- src/app/pipe/date-passed.pipe.ts
+++ src/app/pipe/date-passed.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Seconds } from '../enum/seconds';
 
 @Pipe({
   name: 'datePassed'
@@ -7,61 +8,53 @@ export class DatePassedPipe implements PipeTransform {
 
   transform(value: any): string {
     let date = new Date();
+    // Transform value type from string to Date
     let inputDateString = value.substring(0, value.indexOf('T')).split('-');
-    let inputDate: any = new Date(inputDateString[0], inputDateString[1]-1, inputDateString[2]);
+
     // Get dates without times
     let todayWithNoTime: any = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-
+    let inputDate: any = new Date(inputDateString[0], inputDateString[1]-1, inputDateString[2]);
     // Get date difference in seconds
     let dateDifference = (todayWithNoTime - inputDate) * 0.001;
-    // Seconds in different intervals
-    const seconds = {
-      min: 60,
-      hour: 3600,
-      day: 86400,
-      week: 604800,
-      month: 2629743,
-      year: 31556926
-    }
     let dateCount = '';
 
-    if(dateDifference < seconds.min) {
+    if(dateDifference < Seconds.MIN) {
       dateCount = `seconds ago`;
-    } else if (dateDifference >= seconds.min && dateDifference < seconds.hour){
-      if(Math.floor(dateDifference/seconds.min) === 1){
-        dateCount = `${Math.floor(dateDifference/seconds.min)} minute ago`;
+    } else if (dateDifference >= Seconds.MIN && dateDifference < Seconds.HOUR){
+      if(Math.floor(dateDifference / Seconds.MIN) === 1){
+        dateCount = `${Math.floor(dateDifference / Seconds.MIN)} minute ago`;
       } else {
-        dateCount = `${Math.floor(dateDifference/seconds.min)} minutes ago`;
+        dateCount = `${Math.floor(dateDifference / Seconds.MIN)} minutes ago`;
       }
-    } else if (dateDifference >= seconds.hour && dateDifference < seconds.day){
-      if(Math.floor(dateDifference/seconds.hour) === 1){
-        dateCount = `${Math.floor(dateDifference/seconds.hour)} hour ago`;
+    } else if (dateDifference >= Seconds.HOUR && dateDifference < Seconds.DAY){
+      if(Math.floor(dateDifference / Seconds.HOUR) === 1){
+        dateCount = `${Math.floor(dateDifference / Seconds.HOUR)} hour ago`;
       } else {
-        dateCount = `${Math.floor(dateDifference/seconds.hour)} hours ago`;
+        dateCount = `${Math.floor(dateDifference / Seconds.HOUR)} hours ago`;
       }
-    } else if (dateDifference >= seconds.day && dateDifference < seconds.week){
-      if(Math.floor(dateDifference/seconds.day) === 1){
-        dateCount = `${Math.floor(dateDifference/seconds.day)} day ago`;
+    } else if (dateDifference >= Seconds.DAY && dateDifference < Seconds.WEEK){
+      if(Math.floor(dateDifference/Seconds.DAY) === 1){
+        dateCount = `${Math.floor(dateDifference / Seconds.DAY)} day ago`;
       } else {
-        dateCount = `${Math.floor(dateDifference/seconds.day)} days ago`;
+        dateCount = `${Math.floor(dateDifference / Seconds.DAY)} days ago`;
       }
-    } else if (dateDifference >= seconds.week && dateDifference < seconds.month){
-      if(Math.floor(dateDifference/seconds.week) === 1){
-        dateCount = `${Math.floor(dateDifference/seconds.week)} week ago`;
+    } else if (dateDifference >= Seconds.WEEK && dateDifference < Seconds.MONTH){
+      if(Math.floor(dateDifference / Seconds.WEEK) === 1){
+        dateCount = `${Math.floor(dateDifference / Seconds.WEEK)} week ago`;
       } else {
-        dateCount = `${Math.floor(dateDifference/seconds.week)} weeks ago`;
+        dateCount = `${Math.floor(dateDifference / Seconds.WEEK)} weeks ago`;
       }
-    } else if (dateDifference >= seconds.month && dateDifference < seconds.year){
-      if(Math.floor(dateDifference/seconds.month) === 1){
-        dateCount = `${Math.floor(dateDifference/seconds.month)} month ago`;
+    } else if (dateDifference >= Seconds.MONTH && dateDifference < Seconds.YEAR){
+      if(Math.floor(dateDifference / Seconds.MONTH) === 1){
+        dateCount = `${Math.floor(dateDifference / Seconds.MONTH)} month ago`;
       } else {
-        dateCount = `${Math.floor(dateDifference/seconds.month)} months ago`;
+        dateCount = `${Math.floor(dateDifference / Seconds.MONTH)} months ago`;
       }
     } else {
-      if(Math.floor(dateDifference/seconds.year) === 1){
-        dateCount = `${Math.floor(dateDifference/seconds.year)} year ago`;
+      if(Math.floor(dateDifference / Seconds.YEAR) === 1){
+        dateCount = `${Math.floor(dateDifference / Seconds.YEAR)} year ago`;
       } else {
-        dateCount = `${Math.floor(dateDifference/seconds.year)} years ago`;
+        dateCount = `${Math.floor(dateDifference / Seconds.YEAR)} years ago`;
       }
     }
     return dateCount;
